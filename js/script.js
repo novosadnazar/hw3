0c0b664cbac4babf5cb0e370e8a368f232764f16
novosadnazar <163895331+novosadnazar@users.noreply.github.com>
2025-02-17 18:59:16 +0200

    1101

---
+++ js/script.js
@@ -0,0 +1,12 @@
+const result = 5 + 5 + "5";
+console.log(typeof(result));
+console.log(result);
+const name1 = "my";
+const name2 = "name is";
+const nameResult = `${name1} ${name2} Viktor`;
+console.log(nameResult);
+
+const userName = "Олександро";
+const payMent = "500грн";
+console.log();
+alert(`Дякуємо ${userName} до сплати ${payMent}`);
